88875022bbee9067c3329bf1aa8c93faa696d92f
jose@gmail <jose1212@gmail>
2022-09-21 08:36:47 -0500

    mi primer commit

---
+++ formularios/main.js
@@ -0,0 +1,40 @@
+console.log('Hello World!');
+const imprimir=()=>{
+  var folio=document.querySelector("#folio").value;
+  var aciertos=document.querySelector("#aciertos").value;
+  var correo=document.querySelector("#correo").value;
+  var fecha=document.querySelector("#fecha").
+  value;
+    var nombre=document.querySelector("#nombre"). value;
+  var ap=document.querySelector("#ap").value;
+  var am=document.querySelector("#am").value;
+var dia=document.querySelector("#dia").value;
+var mes=document.querySelector("#mes").value;
+  var año=document.querySelector("#año").value;
+  var lgdn=document.querySelector("#lgdn").value;
+  var curp=document.querySelector("#curp").value;
+  var sexo=document.querySelector("#sexo").value;
+  var alergia=document.querySelector("#alergia").value;
+  var tp=document.querySelector("#tp").value;
+  var ct=document.querySelector("#ct").value;
+  
+ 
+ 
+ 
+  document.querySelector("#c2").innerHTML=folio;
+  document.querySelector("#c1").innerHTML=aciertos;
+  document.querySelector("#c3").innerHTML=correo;
+  document.querySelector("#c4").innerHTML=fecha;
+  document.querySelector("#c5").innerHTML=nombre;
+  document.querySelector("#c6").innerHTML=ap;
+  document.querySelector("#c7").innerHTML=am;
+  document.querySelector("#c8").innerHTML=dia;
+  document.querySelector("#c9").innerHTML=mes;
+  document.querySelector("#c10").innerHTML=año;
+  document.querySelector("#c11").innerHTML=lgdn;
+  document.querySelector("#c12").innerHTML=curp;
+  document.querySelector("#c13").innerHTML=sexo;
+  document.querySelector("#c14").innerHTML=alergia;
+  document.querySelector("#c15").innerHTML=tp;
+  document.querySelector("#c16").innerHTML=ct;
+}
---
+++ horario/main.js
@@ -0,0 +1,78 @@
+
+*{
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+}
+body{
+  background:red;
+}
+table{
+  border:1px solid black;
+  border-collapse:collapse;
+  margin:50px auto;
+  width:90%;
+}
+th{
+  background:purple;
+  color:#fff;
+  font-weight:bolder;
+  font-size:20px;
+  padding:8px;
+}
+td{
+  font-weight: bold;
+  font-size: 14px;
+  padding: 8px;
+  text-align: center;
+}
+table tr:nth-child(even){
+  background:yellow;
+  
+}
+table tr:nth-child(odd){
+  background: blueviolet;
+}
+table tr:hover td{
+  background:skyblue;
+}
+
+.car{
+  border:1px solid white;
+  border-collapse:collapse;
+  margin:50px auto;
+  width:90%;
+}
+th{
+  background:purple;
+  color:#fff;
+  font-weight:bolder;
+  font-size:20px;
+  padding:8px;
+}
+td{
+  font-weight: bold;
+  font-size: 14px;
+  padding: 8px;
+  text-align: center;
+}
+.car td:nth-child(even){
+  background:yellow;
+  
+}
+.car td:nth-child(odd){
+  background: blueviolet;
+}
+.car td:hover{
+  background:skyblue;
+}
+tr{
+  border-bottom: 2px solid black;
+}
+
+
+
+
+.tabla1 td{
+  height:75px;
+}
